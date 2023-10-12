8ee80515f6b8b84cbbac4517d5b1cb3aa3629264
Jan Lewandowski <jan.lewandowski@sailingbyte.com>
2023-10-12 19:21:18 +0200

    fix try2

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,19 +1,7 @@
 export default [
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      headers: "*",
-      origin: [
-        "http://localhost:1337",
-        "http://next13masters.stagetest.it/",
-        "https://next13masters.stagetest.it/",
-        "https://next13-masters-ochre.vercel.app/",
-      ],
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
